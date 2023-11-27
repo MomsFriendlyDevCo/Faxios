b793dd7125f0edfbbd81d315ce218c71670e8c1b
Matt Carter <m@ttcarter.com>
2023-11-27 17:59:50 +1000

    task: Basic docs + proof-of-concept

---
+++ test/index.js
@@ -0,0 +1,56 @@
+import {expect} from 'chai';
+import faxios, {defaults, getFetchRequest} from '#lib/faxios';
+
+describe('@MomsFriendlyDevCo/Faxios', ()=> {
+
+	it('translate basic config structures', ()=> {
+		let data;
+
+		expect(getFetchRequest({
+			url: 'https://google.com',
+		})).to.deep.equal([
+			'https://google.com',
+			{
+				method: 'GET',
+				headers: defaults.headers.common,
+			},
+		]);
+
+		expect(getFetchRequest({
+			method: 'POST',
+			url: 'https://google.com',
+		})).to.deep.equal([
+			'https://google.com',
+			{
+				method: 'POST',
+				headers: defaults.headers.common,
+			},
+		]);
+
+		data = {
+			foo: 123,
+			bar: 'bar!',
+			baz: ['1', false, null, 3],
+		};
+		expect(getFetchRequest({
+			method: 'POST',
+			url: 'https://google.com',
+			data,
+			headers: {
+				Authentication: 'Bearer abcdef',
+			},
+		})).to.deep.equal([
+			'https://google.com',
+			{
+				method: 'POST',
+				body: JSON.stringify(data),
+				headers: {
+					Authentication: 'Bearer abcdef',
+					...defaults.headers.common,
+				},
+			},
+		]);
+
+	});
+
+});
